443c2a0a5743931dd1a12c12c0889dbd54935cf5
Milosz Slezak <miloszoxo1982@gmail.com>
2023-06-26 21:32:06 +0100

    add solution to kata 4

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const { getTopics } = require("./__controllers__/topics.controller");
 const { getAll } = require("./__controllers__/all.controller");
+const { getArticleById } = require("./__controllers__/article.controller");
 const app = express();
 app.use(express.json());
 
 app.get("/api/topics", getTopics);
+app.get("/api/articles/:article_id", getArticleById);
 
 app.all("*", getAll);
 
+app.use((err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Invalid Request" });
+  } else next(err);
+});
+app.use((err, req, res, next) => {
+  if (err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  } else next(err);
+});
 app.use((err, req, res, next) => {
   if (err) {
     console.log(err);
