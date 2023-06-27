a9dffbfd527e25a72406474180dc06a202231071
Milosz Slezak <miloszoxo1982@gmail.com>
2023-06-27 11:09:16 +0100

    last change

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const { getTopics } = require("./controllers/topics.controller");
 const { getAll } = require("./controllers/all.controller");
 const { getApi } = require("./controllers/api.controller");
+const { getArticleById } = require("./controllers/article.controller");
 const app = express();
 app.use(express.json());
 
+app.get("/api", getApi);
 app.get("/api/topics", getTopics);
 app.get("/api/articles/:article_id", getArticleById);
 
--- controllers/article.controller.js
+++ controllers/article.controller.js
@@ -1,4 +1,4 @@
-const { selectArticleById } = require("../__model__/article.model");
+const { selectArticleById } = require("../models/article.model");
 
 exports.getArticleById = (req, res, next) => {
   const { article_id } = req.params;
